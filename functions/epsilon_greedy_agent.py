68283b8a2c78e25e3709ae8eac34c9bef9e47cb1
Thomas Aussaguès <thomas.aussagues@imt-atlantique.net>
2023-03-02 21:00:58 +0100

    Added functions to train epsilon greedy and UCB agents in the non-stationary case

--- functions/epsilon_greedy_agent.py
+++ functions/epsilon_greedy_agent.py
@@ -108,4 +108,80 @@ def train_agent_epsilon_greedy(per_values, bitrates, n_agents=1000, n_iter=1000,
 
     return rewards, action_values, action_counts
 
+
+def train_agent_epsilon_greedy_non_stat(per_values, bitrates, n_agents=1000, n_iter=1000, eps=.05, alpha = None):
+    """Train agents using incrementally computed sample averages and
+    epsilon-greedy action selection in the non-stationary case.
+
+    Parameters
+    ----------
+    per_values: array-like
+        TPER values of each modulation method at each
+        iteration
+    birates: array-like
+        The birates associated to each action
+    n_iter: int, default 1000
+        Number of time steps
+    n_agent: int, default 1000
+        Number of agents
+    eps: float, default 0.05
+        Probability of selecting a non-greedy action instead of the greedy one
+    alpha: float, 
+        Learning rate, if set to None, the sample-average method is used: 
+        the learning rate is set to 1 / number of times action a has been chosen 
+        between first and current iteration
+        
+    Returns
+    -------
+    rewards: array-like
+        rewards for each agent at each iteration
+    action_values: array-like
+        Estimated action values for each agent
+        at each iteration
+    action_counts: array-like
+        Number of action selections for each agent
+        at each iteration
+    """
+
+    ## Initialization
+    # Number of actions ie number of modulation methods
+    k = per_values.shape[1] 
+    # Estimated action values for each agent
+    # at each iteration
+    action_values = np.zeros((n_iter, n_agents, k))
+    # Number of action selections for each agent
+    # at each iteration
+    action_counts = np.zeros((n_iter, n_agents, k)) 
+    # Rewards for each agent at each iteration
+    rewards = np.zeros((n_iter, n_agents))
+   
+    
+    for n_iter_current in range(n_iter) :
+
+        # Iter through every agents
+        for n_agent_current in range(n_agents):
+            
+            # Get next action
+            action = get_action_epsilon_greedy(action_values[n_iter_current, n_agent_current, :], eps)
+            # Get the corresponding reward
+            reward = get_reward(per_values[n_iter_current, :], bitrates, action)
+            # Update the action count
+            action_counts[n_iter_current + 1 :, n_agent_current, action] = action_counts[n_iter_current, n_agent_current, action] + 1
+            
+            # If alpha is set to None, the learning-rate is set to
+            # 1 / number of times action a has been chosen between first 
+            # and current iteration
+            if alpha is None :
+                alpha = 1 / action_counts[n_iter_current, n_agent_current, action] 
+
+            # Update of the action-value estimate for the selected action
+            action_values[n_iter_current + 1 : , n_agent_current, action] =\
+                 action_values[n_iter_current, n_agent_current, action] +\
+                    alpha * (reward - action_values[n_iter_current, n_agent_current, action])
+
+            # Update the mean reward
+            rewards[n_iter_current, n_agent_current] = reward
+
+    return rewards, action_values, action_counts
+
     
--- functions/ucb_agent.py
+++ functions/ucb_agent.py
@@ -115,3 +115,85 @@ def train_ucb_agent(per_values, bitrates, n_agents=1000, n_iter=1000, c=2., alph
             rewards[n_iter_current, n_agent_current] = R
 
     return rewards, action_values, action_counts
+
+
+
+
+def train_ucb_agent_non_stat(per_values, bitrates, n_agents=1000, n_iter=1000, c=2., alpha = None):
+    """Train agents using incrementally computed sample averages and
+    Upper Confidence Band action selection in the non-stationary case.
+
+    Parameters
+    ----------
+   
+    per_values: array-like
+        PER values of each modulation method at each
+        iteration
+    birates: array-like
+        The birates associated to each action
+    n_agent: int, default 1000
+        Number of agents
+    n_iter: int, default 1000
+        Number of time steps
+    eps: float, default 0.05
+        Probability of selecting a non-greedy action instead of the greedy one
+    c: float
+        parameter of the UCB 
+    alpha: float, 
+        Learning rate, if set to None, the sample-average method is used: 
+        the learning rate is set to 1 / number of times action a has been chosen 
+        between first and current iteration
+        
+    Returns
+    -------
+    rewards: array-like
+        rewards for each agent at each iteration
+    action_values: array-like
+        Estimated action values for each agent
+        at each iteration
+    action_counts: array-like
+        Number of action selections for each agent
+        at each iteration
+
+    """
+
+    ## Initialization
+    # Number of actions ie number of modulation methods
+    k = per_values.shape[1]
+    # Estimated action values for each agent
+    # at each iteration
+    action_values = np.zeros((n_iter, n_agents, k))
+    # Number of action selections for each agent
+    # at each iteration
+    action_counts = np.zeros((n_iter, n_agents, k)) 
+    # Rewards for each agent at each iteration
+    rewards = np.zeros((n_iter, n_agents))
+   
+    ## Learning loop
+    for n_iter_current in range(n_iter) :
+
+        # Iter through every agents
+        for n_agent_current in range(n_agents):
+            
+            # Get next action
+            action = get_action_ucb(action_values[n_iter_current, n_agent_current, :], action_counts[n_iter_current, n_agent_current, :], c)
+            # Get the corresponding reward
+            R = get_reward(per_values[n_iter_current, :], bitrates, action)
+            # Update the action count
+            action_counts[n_iter_current + 1 :, n_agent_current, action] = action_counts[n_iter_current, n_agent_current, action] + 1
+            
+            # If alpha is set to None, the learning-rate is set to
+            # 1 / number of times action a has been chosen between first 
+            # and current iteration
+            if alpha is None :
+                alpha = 1 / action_counts[n_iter_current, n_agent_current, action] 
+
+            # Update of the action-value estimate for the selected action
+            action_values[n_iter_current + 1 : , n_agent_current, action] =\
+                 action_values[n_iter_current, n_agent_current, action] +\
+                    alpha * (R - action_values[n_iter_current, n_agent_current, action])
+
+            # Store the reward
+            rewards[n_iter_current, n_agent_current] = R
+
+    return rewards, action_values, action_counts
